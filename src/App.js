8ea39f7c5ad5ad90ebffd5fa8659c5b1adc5fa58
PetiurAliona <alena.petyur@gmail.com>
2021-08-31 15:06:38 +0300

    +hooks App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { ToastContainer } from "react-toastify"
 import ImageGallery from "./components/ImageGallery/ImageGallery"
 import Searchbar from "./components/Searchbar/Searchbar"
 
-class App extends Component {
-  state = { searchQuery: "" }
+const App = () => {
+  const [searchQuery, setSearchQuery] = useState("")
 
-  onNewQuery = ({ searchQuery }) => {
-    this.setState({ searchQuery })
+  const onNewQuery = ({ searchQuery }) => {
+    setSearchQuery(searchQuery)
   }
 
-  render() {
-    const { searchQuery } = this.state
-    return (
-      <>
-        <Searchbar onSubmit={this.onNewQuery} />
-        <ImageGallery searchQuery={searchQuery} />
-        <ToastContainer autoClose={2000} />
-      </>
-    )
-  }
+  return (
+    <>
+      <Searchbar onSubmit={onNewQuery} />
+      <ImageGallery searchQuery={searchQuery} />
+      <ToastContainer autoClose={2000} />
+    </>
+  )
 }
 
 export default App
+
+// class App extends Component {
+//   state = { searchQuery: "" }
+
+//   onNewQuery = ({ searchQuery }) => {
+//     this.setState({ searchQuery })
+//   }
+
+//   render() {
+//     const { searchQuery } = this.state
+//     return (
+//       <>
+//         <Searchbar onSubmit={this.onNewQuery} />
+//         <ImageGallery searchQuery={searchQuery} />
+//         <ToastContainer autoClose={2000} />
+//       </>
+//     )
+//   }
+// }
+
+// export default App
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,49 +1,46 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
 
 import PropTypes from "prop-types"
 
 import styled from "./Searchbar.module.css"
 import { toast } from "react-toastify"
 
-class Searchbar extends Component {
-  state = { searchQuery: "" }
+const Searchbar = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState("")
 
-  onHandleInputChange = (e) => {
-    const { name, value } = e.target
-    this.setState({ [name]: value })
+  const onHandleInputChange = (e) => {
+    setSearchQuery(e.currentTarget.value)
   }
 
-  onSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    if (this.state.searchQuery.trim() === "") {
+    if (searchQuery.trim() === "") {
       return toast.error("Please enter the date")
     }
-    this.props.onSubmit(this.state)
+    onSubmit(searchQuery)
+    setSearchQuery("")
   }
 
-  render() {
-    const { searchQuery } = this.state
-    return (
-      <header className={styled.Searchbar}>
-        <form onSubmit={this.onSubmit} className={styled.SearchForm}>
-          <button type="submit" className={styled.SearchFormButton}>
-            <span className={styled.SearchFormButtonLabel}>Search</span>
-          </button>
+  return (
+    <header className={styled.Searchbar}>
+      <form onSubmit={handleSubmit} className={styled.SearchForm}>
+        <button type="submit" className={styled.SearchFormButton}>
+          <span className={styled.SearchFormButtonLabel}>Search</span>
+        </button>
 
-          <input
-            className={styled.SearchFormInput}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.onHandleInputChange}
-            value={searchQuery}
-            name="searchQuery"
-          />
-        </form>
-      </header>
-    )
-  }
+        <input
+          className={styled.SearchFormInput}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={onHandleInputChange}
+          value={searchQuery}
+          name="searchQuery"
+        />
+      </form>
+    </header>
+  )
 }
 
 Searchbar.propTypes = {
@@ -51,3 +48,50 @@ Searchbar.propTypes = {
 }
 
 export default Searchbar
+
+// class Searchbar extends Component {
+//   state = { searchQuery: "" }
+
+//   onHandleInputChange = (e) => {
+//     const { name, value } = e.target
+//     this.setState({ [name]: value })
+//   }
+
+//   onSubmit = (e) => {
+//     e.preventDefault()
+//     if (this.state.searchQuery.trim() === "") {
+//       return toast.error("Please enter the date")
+//     }
+//     this.props.onSubmit(this.state)
+//   }
+
+//   render() {
+//     const { searchQuery } = this.state
+//     return (
+//       <header className={styled.Searchbar}>
+//         <form onSubmit={this.onSubmit} className={styled.SearchForm}>
+//           <button type="submit" className={styled.SearchFormButton}>
+//             <span className={styled.SearchFormButtonLabel}>Search</span>
+//           </button>
+
+//           <input
+//             className={styled.SearchFormInput}
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.onHandleInputChange}
+//             value={searchQuery}
+//             name="searchQuery"
+//           />
+//         </form>
+//       </header>
+//     )
+//   }
+// }
+
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func,
+// }
+
+// export default Searchbar
